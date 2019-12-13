f82f08f1ea5e3ff914db72201c0ffc3b67f2870c
jhyou <jhyou@192.168.123.102>
2019-12-13 15:55:39 +0900

    [team04_Jo-Huiyeong] 왼쪽사이드바오류수정_2019-12-13

--- shop.fims/src/main/java/shop/fims/human/controller/HumanController.java
+++ shop.fims/src/main/java/shop/fims/human/controller/HumanController.java
@@ -7,26 +7,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HumanController {
 
 	//인적사항 리스트
-	@GetMapping("/human/humanList")
+	@GetMapping("/humanList")
 	public String selectHuman() {
 		
 		return "/human/humanList";
 	}
 	//인적사항 등록
-	@GetMapping("/human/humanInsert")
+	@GetMapping("/humanInsert")
 	public String insertHuman() {
 		
 		return "/human/humanInsert";
 	}
 	
 	//인적사항 수정
-	@GetMapping("/human/humanUpdate")
+	@GetMapping("/humanUpdate")
 	public String updateHuman() {
 		
 		return "/human/humanUpdate";
 	}
 	//인적사항 삭제
-	@GetMapping("/human/humanDelete")
+	@GetMapping("/humanDelete")
 	public String delectHuman() {
 		
 		return "/human/humanDelete";
--- shop.fims/src/main/java/shop/fims/human/controller/WorkController.java
+++ shop.fims/src/main/java/shop/fims/human/controller/WorkController.java
@@ -7,55 +7,55 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WorkController {
 
 	//축제업무별배치인원리스트
-	@GetMapping("/work/workingpeopleList")
+	@GetMapping("/workingpeopleList")
 	public String selectWorkingpeople() {
 		
 		return "/work/workingpeopleList";
 	}
 	//축제업무분류리스트
-	@GetMapping("/work/workcategoryList")
+	@GetMapping("/workcategoryList")
 	public String selectWorkCategory() {
 		
 		return "/work/workcategoryList";
 	}
 	//축제업무분류등록
-	@GetMapping("/work/workcategoryInsert")
+	@GetMapping("/workcategoryInsert")
 	public String insertWorkCategory() {
 		
 		return "/work/workcategoryInsert";
 	}
 	//축제업무분류수정
-	@GetMapping("/work/workcategoryUpdate")
+	@GetMapping("/workcategoryUpdate")
 	public String updateWorkCategory() {
 		
 		return "/work/workcategoryUpdate";
 	}
 	//축제업무분류삭제
-	@GetMapping("/work/workcategoryDelete")
+	@GetMapping("/workcategoryDelete")
 	public String delectWorkCategory() {
 		
 		return "/work/workcategoryDelete";
 	}
 	//축제업무리스트
-	@GetMapping("/work/workdivisionList")
+	@GetMapping("/workdivisionList")
 	public String selectWorkDivision() {
 		
 		return "/work/workdivisionList";
 	}
 	//축제업무등록
-	@GetMapping("/work/workdivisionInsert")
+	@GetMapping("/workdivisionInsert")
 	public String insertWorkDivision() {
 		
 		return "/work/workdivisionInsert";
 	}
 	//축제업무수정
-	@GetMapping("/work/workdivisionUpdate")
+	@GetMapping("/workdivisionUpdate")
 	public String updateWorkDivision() {
 		
 		return "/work/workdivisionUpdate";
 	}
 	//축제업무삭제
-	@GetMapping("/work/workdivisionDelete")
+	@GetMapping("/workdivisionDelete")
 	public String delectWorkDivision() {
 		
 		return "/work/workdivisionDelete";
