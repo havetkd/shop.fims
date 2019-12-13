fdfb01d68b9c354759349b8c1642f7c759263d48
smart-202-10 <smart-202-10@smart-202-10-PC>
2019-12-13 12:45:52 +0900

    [team04_Kim-Do-Min] 평가관리 - 신청서 화면 연결 완료 _ 2019-12-13

--- shop.fims/src/main/java/shop/fims/festival_announcement/controller/FesAnnController.java
+++ shop.fims/src/main/java/shop/fims/festival_announcement/controller/FesAnnController.java
@@ -6,6 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FesAnnController {
 	
+	
+	/*
+	 * @file   FesAnnController.java
+	 * @name   FesAnnController
+	 * @brief  공고 관련 
+	 * @author ksmart33 김도민
+	 */
+	
+	//공고 화면 관련 메서드
 	@GetMapping("/festival_announcement/announcementView")
 	public String AnnouncementView() {
 		return "festival_announcement/announcementView";
--- shop.fims/src/main/java/shop/fims/festival_committee/controller/CommitteeController.java
+++ shop.fims/src/main/java/shop/fims/festival_committee/controller/CommitteeController.java
@@ -6,6 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CommitteeController {
 	
+	/*
+	 * @file   CommitteeController.java
+	 * @name   CommitteeController
+	 * @brief  위원회 관련 
+	 * @author ksmart33 김도민
+	 */
+	
+	//위원회화면관련메서드
 	@GetMapping("/festival_committee/allCommitteeView")
 	public String AllCommitteeView() {
 		return "festival_committee/allCommitteeView";
